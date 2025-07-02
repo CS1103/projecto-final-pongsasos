cacd6dfdc59b051cfaaa205dc02e511bdca3f48b
maffzz <maria.lazon@utec.ued.pe>
2025-07-02 07:50:26 -0500

    feat: screen

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <raylib.h>
+using namespace std;
+
+const int screen_width = 1200;
+const int screen_height = 800;
+
+class Ball {
+public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
+
+    void Draw() {
+        DrawCircle(x, y, radius, WHITE);
+    }
+
+    void Update() {
+        x += speed_x;
+        y += speed_y;
+    }
+};
+
+int main() {
+    InitWindow(screen_width, screen_height, "PONGSASOOO");
+    SetTargetFPS(60);
+    Ball ball;
+    ball.radius = 20;
+    ball.x = screen_width/2;
+    ball.y = screen_height/2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ball.Update();
+        ClearBackground(BLANK);
+        DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
+        ball.Draw();
+        DrawRectangle(10, screen_height/2 - 60, 25,120, WHITE);
+        DrawRectangle(screen_width-35, screen_height/2 - 60, 25,120, WHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
